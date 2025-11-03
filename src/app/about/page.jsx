64851638dd778dfa4214c0f8dd0c83ea8d31d5ce
neggmmm <kekkdead@gmail.com>
2025-11-03 10:46:01 +0200

    fixing about

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,15 +1,7 @@
-import Image from "next/image";
-import { useDarkMode } from "../context/DarkModeContext";
-import ProfileCard from "../Components/ProfileCard";
-
 export default function About() {
-    const { darkMode, setDarkMode } = useDarkMode();
     return (
-        <div className="h-screen overflow-hidden border-top rounded-xl sticky top-0">
+        <div className=" overflow-hidden border-top rounded-xl sticky top-0">
             <div className="grid grid-cols-5">
-
-
-
             </div>
         </div>
 
