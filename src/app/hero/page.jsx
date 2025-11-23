9560f3a86102d02c305ade4524272c2861658016
neggmmm <kekkdead@gmail.com>
2025-11-23 19:44:11 +0200

    hero section modifying

--- src/app/hero/page.jsx
+++ src/app/hero/page.jsx
@@ -8,27 +8,17 @@ import CircularText from "../Components/CricularText";
 import { hover } from "motion";
 
 export default function Hero() {
-  const { darkMode } = useDarkMode();
-  const [hoveredLinkedIn, setHoveredLinkedIn] = useState(false);
-  const [hoveredGitHub, setHoveredGitHub] = useState(false);
+
   return (
 
     <div id="hero" className="h-screen overflow-hidden select-none">
 
       <div className="grid grid-cols-3 my-30 h-[70%]">
         <div className="col-span-2 flex flex-col justify-between">
-          <div className="flex flex-col justify-center mx-20 w-full  goldman-regular">
-            <TextPressure
-              text="Abdalkareem Negm"
-              flex={true}
-              alpha={false}
-              stroke={false}
-              width={true}
-              weight={true}
-              textColor="black"
-              strokeColor="#ff0000"
-              minFontSize={200}
-            />
+          <div className="flex flex-col justify-center mx-20 w-full h-full goldman-regular">
+            <div className="orbitron-reg text-8xl">
+              Abdalkareem <span className="orbitron-600">NEGM</span>
+              </div>
             <div className="text-3xl flex content-between justify-between flex-col">
               <div className="">
                 <span>Software Engineer </span>|<span> Web Developer</span>
