dfbae97c45a98a4dce7651a07294361be6b64372
neggmmm <kekkdead@gmail.com>
2026-01-19 17:26:38 +0200

    hero section responsive

--- src/app/hero/page.jsx
+++ src/app/hero/page.jsx
@@ -16,19 +16,11 @@ export default function Hero() {
     <div id="hero" className="h-screen overflow-hidden select-none">
 
       <div className="grid grid-cols-3 my-30 h-[70%]">
-        <div className="col-span-2 flex flex-col justify-between">
-          <div className="flex flex-col justify-center mx-20 w-full  goldman-regular">
-            <TextPressure
-              text="Abdalkareem Negm"
-              flex={true}
-              alpha={false}
-              stroke={false}
-              width={true}
-              weight={true}
-              textColor="black"
-              strokeColor="#ff0000"
-              minFontSize={200}
-            />
+        <div className="col-span-2 flex flex-col">
+          <div className="flex flex-col justify-center my-30 md:my-5 mx-20 w-full  goldman-regular">
+            <div className="text-8xl orbitron-regular">
+              <span className="hidden lg:block">Abdalkareem</span> <span className="text-brand orbitron-bold block ">NEGM</span>
+            </div>
             <div className="text-3xl flex content-between justify-between flex-col">
               <div className="">
                 <span>Software Engineer </span>|<span> Web Developer</span>
@@ -50,7 +42,7 @@ export default function Hero() {
               document.querySelector("#contactUs")?.scrollIntoView();
             }}
           >
-            <div className="absolute bottom-40 right-40">
+            <div className="absolute bottom-40 right-30 md:right-40">
               <CircularText
                 text="OPEN TO WORK | FULLSTACK | "
                 onHover="pause"
